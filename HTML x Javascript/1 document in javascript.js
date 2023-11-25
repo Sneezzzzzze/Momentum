0a1d9c673ef6118888dedeed4b9a8355c16b5683
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2023-11-25 15:39:34 +0700

    html x javascript

---
+++ HTML x Javascript/1 document in javascript.js
@@ -0,0 +1,8 @@
+// Document Object
+// javascript already connect with HTML
+// javascript can access the HTML element by using document object
+
+document.title //-> Show the title of the page that write in the <title> tag (HTML)
+
+// And we can change the title of the page by using this property
+document.title = "New Title";
---
+++ HTML x Javascript/2 grab elements.js
@@ -0,0 +1,10 @@
+//Grab elements by ID
+
+const title = document.getElementById("title"); //-> Grab the element that have id="title"
+
+console.dir(title); //-> Show the title element in the console
+
+title.innerText = "Got you!"; // -> Change the text of the title element
+
+console.log(title.id);
+console.log(title.className);
