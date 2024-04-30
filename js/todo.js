ff25d865907d0d93ad9a7d6af0136523f05df6ca
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2024-04-30 11:28:38 +0700

    delete todo

--- js/todo.js
+++ js/todo.js
@@ -2,16 +2,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) { // 삭제 버튼을 누르면 실행되는 함수 event = 이벤트 객체 
+  const li = event.target.parentElement; // button의 부모인 li를 찾아서
+  li.remove(); // 삭제
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newToDo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
-  event.preventDefault();
+  event.preventDefault(); // 이벤트의 기본 동작을 막음 (새로고침)
   const newToDo = toDoInput.value;
   toDoInput.value = "";
   paintToDo(newToDo);
